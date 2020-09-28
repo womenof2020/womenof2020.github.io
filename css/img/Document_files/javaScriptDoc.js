767a979401fc8d77aa8a10c4735f4b170032bb3e
Jasmine Allen <67152674+JasminedAllen@users.noreply.github.com>
2020-09-28 12:52:23 +0100

    updates

--- css/img/Document_files/javaScriptDoc.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    
-    var newsLetter = document.getElementById('submitButton');
-    console.log(newsLetter);
-    newsLetter.addEventListener('click', function () {
-        alert('You have been successfully subscribe to our news letter!');
-    })
-})
